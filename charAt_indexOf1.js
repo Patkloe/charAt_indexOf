12ced2fadb24d3481f832f5c869d49851a4c1209
Patkloe <52906039+Patkloe@users.noreply.github.com>
2021-05-13 08:46:40 -0500

    Update charAt_indexOf1.js

--- charAt_indexOf1.js
+++ charAt_indexOf1.js
@@ -5,3 +5,15 @@ function str_charat_index(x,s){
  alert('A la position ' + ' ' + ind + ' ' + 'nous avons le charactere :' + ' ' + pos);
 }
 str_charat_index(0,'Patrick');
+// version 1.0
+
+function str_charat_index(x,s){
+ var pos = s.charAt(x);
+ alert('caractere' +  pos + ' a la Position ' + ' ' + x + 'dans string'+ ' ' +s );
+ var ind = s.indexOf(pos);
+ alert('A la position ' + ' ' + ind + ' ' + 'nous avons le charactere :' + ' ' + pos);
+}
+str_charat_index(0,'Patrick');
+/**  test commentaires multilignes
+ Comment ca marche
+**/
